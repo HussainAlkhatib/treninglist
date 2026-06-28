50f894e696dbaccba553723a79089170ac52d0d3
hussain_syrer <147648308+HussainAlkhatib@users.noreply.github.com>
2026-06-28 21:56:12 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,22 @@
-const CACHE_NAME = 'trening-cache-v1';
-const ASSETS = [
-  'index.html',
-  'manifest.json'
-];
+const CACHE = 'ukeplan-v1';
+const FILES = ['index.html', 'manifest.json'];
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
-  );
+self.addEventListener('install', e => {
+    e.waitUntil(
+        caches.open(CACHE).then(c => c.addAll(FILES))
+    );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
-    })
-  );
+self.addEventListener('activate', e => {
+    e.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)));
+        })
+    );
+});
+
+self.addEventListener('fetch', e => {
+    e.respondWith(
+        caches.match(e.request).then(r => r || fetch(e.request))
+    );
 });
